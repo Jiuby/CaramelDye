b132ed02070033e491c324df629306e6486a77b4
Juan Velez <mrjiuby@gmail.com>
2024-07-16 21:09:02 -0500

    added: Se modifica un poco factura

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -141,8 +142,11 @@ STATICFILES_DIRS = [
 ]
 
 # media files configuration
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 from django.contrib.messages import constants as messages
