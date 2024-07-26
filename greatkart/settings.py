cbf04932312557505ebdad2d7b55eb8195b4714e
Juan Velez <mrjiuby@gmail.com>
2024-07-26 01:42:30 -0500

    Deployment

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -32,6 +32,7 @@ DEBUG = config('DEBUG', default=False, cast=bool) # True
 ALLOWED_HOSTS = ["carameldye.azurewebsites.net", "http://127.0.0.1:0000/"]
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
